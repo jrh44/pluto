a8a38a7d032392e6d7c9fcf6ac1eb8b47a7836e8
Jon <j.harewen@gmail.com>
2024-06-21 21:15:46 +0100

    First attempt

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -1,3 +1,9 @@
+resource "google_cloudbuildv2_repository" "my-repository" {
+  name = "my-repo"
+  parent_connection = google_cloudbuildv2_connection.my-connection.id
+  remote_uri = "https://github.com/jrh44/pluto.git"
+}
+
 resource "google_cloudbuild_trigger" "trigger" {
   name = "pluto-cloud-function-trigger"
 

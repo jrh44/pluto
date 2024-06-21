080982620ff289d274a836b38da8a561a96fd706
Jon <j.harewen@gmail.com>
2024-06-21 15:57:28 +0100

    Set buckets to force destroy

--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -2,10 +2,12 @@ resource "google_storage_bucket" "pluto_bucket" {
   project  = var.project_id
   name     = "${var.project_id}${var.bucketsuffix}"
   location = "US"
+  force_destroy = true
 }
 
 resource "google_storage_bucket" "pluto_source" {
   project  = var.project_id
   name     = "${var.project_id}${var.sourcebucketsuffix}"
   location = "US"
+  force_destroy = true
 }

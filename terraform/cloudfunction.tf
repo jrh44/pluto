d714368f9fa984e32cd0d093f2586e78a6fd1e71
Jon <j.harewen@gmail.com>
2024-06-21 13:19:00 +0100

    Removed redundant code block?

--- terraform/cloudfunction.tf
+++ terraform/cloudfunction.tf
@@ -13,12 +13,12 @@ resource "null_resource" "fetch_and_zip_source" {
   }
 }
 
-resource "google_storage_bucket_object" "function_zip" {
-  name   = "function_source.zip"
-  bucket = google_storage_bucket.pluto_source.name
-  source = "/tmp/function_source.zip"
-  depends_on = [null_resource.fetch_and_zip_source]
-}
+#resource "google_storage_bucket_object" "function_zip" {
+#  name   = "function_source.zip"
+#  bucket = google_storage_bucket.pluto_source.name
+#  source = "/tmp/function_source.zip"
+#  depends_on = [null_resource.fetch_and_zip_source]
+#}
 
 resource "google_cloudfunctions_function" "function" {      
   project             = var.project_id
@@ -36,5 +36,5 @@ resource "google_cloudfunctions_function" "function" {
     resource   = google_pubsub_topic.topic.name
     event_type = "google.pubsub.topic.publish"
   }
-  depends_on = [google_pubsub_topic.topic,null_resource.fetch_and_zip_source,google_storage_bucket_object.function_zip]
+  depends_on = [google_pubsub_topic.topic,null_resource.fetch_and_zip_source]
 }

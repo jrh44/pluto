23e8e72d75db642b28f37baa09810551a1ca4727
Jon <j.harewen@gmail.com>
2024-06-21 12:54:36 +0100

    Try this local exec bs again

--- terraform/cloudfunction.tf
+++ terraform/cloudfunction.tf
@@ -1,14 +1,15 @@
 resource "null_resource" "fetch_and_zip_source" {
   provisioner "local-exec" {
-    command = <<-EOT
-      mkdir -p /tmp/function_source
-      curl -L -o /tmp/main.zip "https://github.com/${var.github_username}/${var.repo_name}/archive/refs/heads/main.zip"
-      unzip /tmp/main.zip -d /tmp/function_source
-      cd /tmp/function_source/cloudfunction
-      zip -r /tmp/function_source.zip .
-      gsutil cp /tmp/function_source.zip "gs://${google_storage_bucket.pluto_source.name}/function_source.zip"
-    EOT
-    interpreter = ["/bin/sh" ,"-c"]
+    command = "bash -c 'mkdir -p /tmp/function_source && curl -L -o /tmp/main.zip https://github.com/${var.github_username}/${var.repo_name}/archive/refs/heads/main.zip && unzip /tmp/main.zip -d /tmp/function_source && cd /tmp/function_source/pluto-main/cloudfunction && zip -r /tmp/function_source.zip . && gsutil cp /tmp/function_source.zip gs://${google_storage_bucket.pluto_source.name}/function_source.zip'"
+    #command = <<-EOT
+    #  mkdir -p /tmp/function_source
+    #  curl -L -o /tmp/main.zip "https://github.com/${var.github_username}/${var.repo_name}/archive/refs/heads/main.zip"
+    #  unzip /tmp/main.zip -d /tmp/function_source
+     # cd /tmp/function_source/pluto-main/cloudfunction
+     # zip -r /tmp/function_source.zip .
+      #gsutil cp /tmp/function_source.zip "gs://${google_storage_bucket.pluto_source.name}/function_source.zip"
+    #EOT
+   # interpreter = ["/bin/sh" ,"-c"]
   }
 }
 

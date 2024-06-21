2b6b88c2f625efb009520113f485fbe381c81cb1
Jon <j.harewen@gmail.com>
2024-06-21 13:45:11 +0100

    Added permissions to serviceaccount

--- terraform/serviceaccounts.tf
+++ terraform/serviceaccounts.tf
@@ -8,3 +8,20 @@ resource "google_project_iam_member" "function_sa_binding" {
   member        = "serviceAccount:${google_service_account.service_account.email}"
 }
 
+resource "google_project_iam_member" "cloudbuild_service_account_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "cloudbuild_cloudfunctions_developer" {
+  project = var.project_id
+  role    = "roles/cloudfunctions.developer"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "cloudbuild_storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}

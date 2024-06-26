f0f6f31956766b2dfa94a7316629f5bea4c61b65
Jon <j.harewen@gmail.com>
2024-06-26 13:56:45 +0100

    Removed the default service account

--- terraform/serviceaccounts.tf
+++ terraform/serviceaccounts.tf
@@ -46,40 +46,40 @@ resource "google_project_iam_member" "objectAdmin" {
 }
 
 
-resource "google_project_iam_member" "cloudbuild_service_account_user" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-}
+#resource "google_project_iam_member" "cloudbuild_service_account_user" {
+#  project = var.project_id
+#  role    = "roles/iam.serviceAccountUser"
+#  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
+#}
 
-resource "google_project_iam_member" "cloudbuild_cloudfunctions_developer" {
-  project = var.project_id
-  role    = "roles/cloudfunctions.developer"
-  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-}
+#resource "google_project_iam_member" "cloudbuild_cloudfunctions_developer" {
+#  project = var.project_id
+#  role    = "roles/cloudfunctions.developer"
+#  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
+#}
 
-resource "google_project_iam_member" "cloudbuild_storage_admin" {
-  project = var.project_id
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-}
+#resource "google_project_iam_member" "cloudbuild_storage_admin" {
+#  project = var.project_id
+#  role    = "roles/storage.admin"
+#  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
+#}
 
-resource "google_project_iam_member" "cloudbuild_editor" {
-  project = var.project_id
-  role    = "roles/cloudbuild.builds.editor"
-  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-}
+#resource "google_project_iam_member" "cloudbuild_editor" {
+#  project = var.project_id
+#  role    = "roles/cloudbuild.builds.editor"
+#  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
+#}
 
-resource "google_project_iam_member" "cloudbuild_logwriter" {
-  project = var.project_id
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-}
+#resource "google_project_iam_member" "cloudbuild_logwriter" {
+#  project = var.project_id
+#  role    = "roles/logging.logWriter"
+#  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
+#}
 
 
-resource "google_project_iam_member" "cloudbuild_objectAdmin" {
-  project = var.project_id
-  role    = "roles/storage.objectAdmin"
-  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-}
+#resource "google_project_iam_member" "cloudbuild_objectAdmin" {
+#  project = var.project_id
+#  role    = "roles/storage.objectAdmin"
+#  member  = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
+#}
 

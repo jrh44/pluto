e48c1efc3d7c91d7e9801c7834c69929234207ae
Jon <j.harewen@gmail.com>
2024-06-28 16:12:23 +0100

    add tform to ignore

--- terraform/cloudfunction.tf
+++ terraform/cloudfunction.tf
@@ -1,17 +1,17 @@
-#resource "null_resource" "fetch_and_zip_source" {
-#  provisioner "local-exec" {
-#    command = "bash -c 'mkdir -p /tmp/function_source && curl -L -o /tmp/main.zip https://github.com/${var.github_username}/${var.repo_name}/archive/refs/heads/main.zip && unzip -o /tmp/main.zip -d /tmp/function_source && cd /tmp/function_source/pluto-main/cloudfunction && zip -r /tmp/function_source.zip . && gsutil cp /tmp/function_source.zip gs://${google_storage_bucket.pluto_source.name}/function_source.zip'"
-#    #command = <<-EOT
-#    #  mkdir -p /tmp/function_source
-#    #  curl -L -o /tmp/main.zip "https://github.com/${var.github_username}/${var.repo_name}/archive/refs/heads/main.zip"
-#    #  unzip /tmp/main.zip -d /tmp/function_source
-#     # cd /tmp/function_source/pluto-main/cloudfunction
-#     # zip -r /tmp/function_source.zip .
-#      #gsutil cp /tmp/function_source.zip "gs://${google_storage_bucket.pluto_source.name}/function_source.zip"
-#    #EOT
-#   # interpreter = ["/bin/sh" ,"-c"]
-#  }
-#}
+resource "null_resource" "fetch_and_zip_source" {
+  provisioner "local-exec" {
+    command = "bash -c 'mkdir -p /tmp/function_source && curl -L -o /tmp/main.zip https://github.com/${var.github_username}/${var.repo_name}/archive/refs/heads/main.zip && unzip -o /tmp/main.zip -d /tmp/function_source && cd /tmp/function_source/pluto-main/cloudfunction && zip -r /tmp/function_source.zip . && gsutil cp /tmp/function_source.zip gs://${google_storage_bucket.pluto_source.name}/function_source.zip'"
+    #command = <<-EOT
+    #  mkdir -p /tmp/function_source
+    #  curl -L -o /tmp/main.zip "https://github.com/${var.github_username}/${var.repo_name}/archive/refs/heads/main.zip"
+    #  unzip /tmp/main.zip -d /tmp/function_source
+     # cd /tmp/function_source/pluto-main/cloudfunction
+     # zip -r /tmp/function_source.zip .
+      #gsutil cp /tmp/function_source.zip "gs://${google_storage_bucket.pluto_source.name}/function_source.zip"
+    #EOT
+   # interpreter = ["/bin/sh" ,"-c"]
+  }
+}
 
 ##resource "null_resource" "set_cloud_build_service_account" {
 ##  provisioner "local-exec" {

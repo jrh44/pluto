bb4d6fb94ed65ab8658e0ba8ec64141388bbb0f9
Jon <j.harewen@gmail.com>
2024-06-28 16:18:08 +0100

    dfghfg

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -1,16 +1,16 @@
-resource "google_cloudbuildv2_connection" "my-connection" {
-  location = "us-central1"
-  name = "my-terraform-github-connection"
-  github_config {
-    
-  }
-}
+#resource "google_cloudbuildv2_connection" "my-connection" {
+#  location = "us-central1"
+#  name = "my-terraform-github-connection"
+#  github_config {
+#    
+#  }
+#}
 
-resource "google_cloudbuildv2_repository" "my-repository" {
-  name = "my-repo"
-  parent_connection = google_cloudbuildv2_connection.my-connection.id
-  remote_uri = "https://github.com/jrh44/pluto.git"
-}
+#resource "google_cloudbuildv2_repository" "my-repository" {
+#  name = "my-repo"
+#  parent_connection = google_cloudbuildv2_connection.my-connection.id
+#  remote_uri = "https://github.com/jrh44/pluto.git"
+#}
 
 resource "google_cloudbuild_trigger" "trigger" {
   name = "pluto-cloud-function-trigger"
@@ -19,7 +19,7 @@ resource "google_cloudbuild_trigger" "trigger" {
     owner        = "jrh44"
     name         = "pluto"
     push {
-      branch = "main"
+      branch = "^main$"
     }
   }
 
